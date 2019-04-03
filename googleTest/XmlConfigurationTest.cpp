315a3ade6da27805bdef4da1a8d1ccde34dd01e7
月時 <x408859786@126.com>
2019-04-03 21:52:32 +0800

    Add tinyxml2 dll

---
+++ googleTest/XmlConfigurationTest.cpp
@@ -0,0 +1,50 @@
+#include "stdafx.h"
+#ifdef GTEST
+#include "gtest/gtest.h"
+#include "Log.h"
+#include "Data.h"
+#include "tinyxml2/tinyxml2.h"
+#include <list>
+
+//#pragma comment(lib,"tinyxml2.lib")
+
+
+using namespace tinyxml2;
+using namespace std;
+
+
+TEST(TinyXml2, ConfigurationTest)
+{
+	EXPECT_TRUE(true);
+
+
+	XMLDocument doc;
+	char* name = "E:\\ZMC\\Code\\C#\\MUL\\TestResults\\test.xml";
+	XMLError result = doc.LoadFile(name);
+	EXPECT_EQ(0, doc.ErrorID());
+
+
+	XMLElement* root = doc.FirstChildElement();
+
+	//Attribute name and value
+	cout << root->Name() << ":" << root->FirstAttribute()->Name() << "=" << root->FirstAttribute()->Value() << endl;
+
+	for (XMLElement* mod = root->FirstChildElement();mod != nullptr;mod = mod->NextSiblingElement())
+	{
+		cout << '\t' << mod->Name() << endl;
+		for (XMLElement* card = mod->FirstChildElement();card != nullptr;card = card->NextSiblingElement())
+		{
+			cout << "\t\t" << card->Name() << ":" << card->FirstAttribute()->Name() << "=" << card->FirstAttribute()->Value() << endl;
+			for (XMLElement*leaf = card->FirstChildElement();leaf != nullptr;leaf = leaf->NextSiblingElement())
+			{
+				cout << "\t\t\t" << leaf->Value() << ":" << (leaf->GetText() ? leaf->GetText() : "NotLeaf") << endl;
+			}
+		}
+	}
+
+	auto card = root->FirstChildElement();
+	cout << card->Name();
+
+	doc.Print();
+}
+#endif
